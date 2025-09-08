ee5410dd422869abbd891c3d61bebb6278d7722e
JadesonBruno <jadesonbruno.a@outlook.com>
2025-09-08 08:19:40 -0300

    feat: Add output personalizado para conexão ssh à instância da api

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,8 @@ output "ml_api_public_dns" {
   description = "Public DNS of the EC2 instance for the ML API"
   value = module.api.ml_api_public_dns
 }
+
+output "ssh_connection_command" {
+  description = "SSH command to connect to the EC2 instance"
+  value = "ssh -i '${var.project_name}-${var.environment}-ml-api-key.pem' ec2-user@${module.api.ml_api_public_dns}"
+}
